77e1b29b161b1a4b1fdf7e03d9c8da23ef3f5f28
Piotr Bielicki <piotrbi3licki@gmail.com>
2024-10-15 15:19:15 +0200

    adding tet_put.py

--- Play/test_put.py
+++ Play/test_put.py
@@ -3,14 +3,12 @@ from random_data import data, name, email
 
 
 def test_response(playwright: sync_playwright):
+    username = data["name"]
     context = playwright.request.new_context(base_url="https://gorest.co.in")
-    response = context.post(url="/public/v2/users", headers={
+    response = context.put(url="/public/v2/users/7472682", headers={
         "Authorization": "Bearer 05594595e5cc386888b43c0457f6f97ecd5ebb6b0e108c65015fc7e45f4bdda4"
     }, data=data)
     res = response.json()
-    print(response)
+    print(res)
 
-    assert response.status == 201
-    assert res["name"] == name
-    assert res["email"] == email
-    print(res["id"])
+    assert response.status == 200
