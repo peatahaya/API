6ca10fb2684b9a522e90a9a4700563c6af501c5e
Piotr Bielicki <piotrbi3licki@gmail.com>
2024-10-15 13:11:14 +0200

    adding new python file

--- Play/test_get_method.py
+++ Play/test_get_method.py
@@ -4,3 +4,15 @@ def test_get(playwright: sync_playwright):
     context = playwright.request.new_context()
     response = context.get(url="https://gorest.co.in/public/v2/users/7471651")
     print(response)
+    assert response.status == 200
+    assert response.status_text == 'OK'
+    print(response.json()["name"])
+    print(response.json()["email"])
+    print(response.json()["gender"])
+    print(response.json()["status"])
+    res = response.json()
+    # First method of asserting a name
+    assert response.json()["name"] == "Digambar Talwar Jr."
+    # Second method of asserting a name
+    assert res.get("name") == "Digambar Talwar Jr."
+    assert response.headers["content-type"] == "application/json; charset=utf-8"
